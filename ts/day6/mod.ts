b6cee14f8e92d44aa220914c3dfa0357cff7e404
scarf <greenscarf005@gmail.com>
2023-12-07 16:35:58 +0900

    feat: day 6 part 2

--- ts/day6/mod.ts
+++ ts/day6/mod.ts
@@ -1,14 +1,12 @@
-import { typedRegEx } from "$typed_regex/mod.ts"
 import { assertEquals } from "$std/assert/assert_equals.ts"
 
 export type Race = { time: number; distance: number }
-
 /**
  * @param time total timespan
  * @param accTime time spent accelerating
  */
-export const travel = (time: number) => (accTime: number): number => (time - accTime) * accTime
 
+export const travel = (time: number) => (accTime: number): number => (time - accTime) * accTime
 Deno.test("travel", () => {
 	const race = travel(7)
 
@@ -24,38 +22,50 @@ Deno.test("travel", () => {
 // Math.ceil(7 / 2) = 4, 4 - 2 = 2, 2 * 2 = 4
 // Math.ceil(30 / 2) = 15, 15 - 11 = 4, 4 * 2 = 8
 // 1~10, {11 ~ 19} 20~30
-
 export const wins = ({ time, distance }: Race) => {
-	// not accelerating at all and accelerating the whole time always loses
-	const cases = Array.from({ length: time - 1 }, (_, i) => i + 1)
 	const tv = travel(time)
-	const shortest = cases.find((t) => tv(t) > distance)!
-    const longest = cases.reverse().find((t) => tv(t) > distance)!
-    const totalWins = longest - shortest + 1
+	let shortest = 0
+	let longest = 0
+	for (let i = 1000000; i < 20000000; i++) {
+		if (tv(i) > distance) {
+			shortest = i
+			console.log({ i, win: tv(i), distance })
+			break
+		}
+	}
+	for (let i = 49979494; i > 20000000; i--) {
+		if (tv(i) > distance) {
+			longest = i
+			console.log({ i, win: tv(i), distance })
+			break
+		}
+	}
+	console.log({ shortest, longest }, longest - shortest + 1)
+	return 0
+	// not accelerating at all and accelerating the whole time always loses
+	// const cases = Array.from({ length: time - 1 }, (_, i) => i + 1)
+	// const shortest = cases.find((t) => tv(t) > distance)!
+	// const longest = cases.reverse().find((t) => tv(t) > distance)!
+	// const totalWins = longest - shortest + 1
 
-	// console.log({ cases, shortest, longest, totalWins })
-    return totalWins
+	// // console.log({ cases, shortest, longest, totalWins })
+	// return totalWins
+	return 0
 }
-
 // const races = [
 // 	{ time: 7, distance: 9 },
 // 	{ time: 15, distance: 40 },
 // 	{ time: 30, distance: 200 },
 // ]
+// const races = [
+// 	{ time: 49, distance: 263 },
+// 	{ time: 97, distance: 1532 },
+// 	{ time: 94, distance: 1378 },
+// 	{ time: 94, distance: 1851 },
+// ]
+// const result = races.map(wins).reduce((a, b) => a * b, 1)
+// console.log(result)
 
-const races = [
-	{ time: 49, distance: 263 },
-	{ time: 97, distance: 1532 },
-	{ time: 94, distance: 1378 },
-    { time: 94, distance: 1851 },
-]
-
-const result = races.map(wins).reduce((a, b) => a * b, 1)
-console.log(result)
-
-// find the first travel distance that exceeds distance requirement
-
-// const parser = (x: string)
-
-// const time = [7, 15, 30]
-// const distance = [9, 40, 200]
+// const race = { time: 71530, distance: 940200 }
+const race = { time: 49979494, distance: 263153213781851 }
+console.log(wins(race))

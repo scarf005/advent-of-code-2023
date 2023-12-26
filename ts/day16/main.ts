ae7aecb694935860a70816b00a055abc6d47b2fe
scarf <greenscarf005@gmail.com>
2023-12-26 18:03:43 +0900

    feat: day 16 part 2

--- ts/day16/main.ts
+++ ts/day16/main.ts
@@ -1,17 +1,8 @@
 import outdent from "$outdent/mod.ts"
-import { Dim, Dir, input, Pos } from "$utils/mod.ts"
-import { BiMap } from "$rimbu/bimap/mod.ts"
+import { Dim, Dir, id, input, Pos } from "$utils/mod.ts"
 import { ArrayNonEmpty } from "$rimbu/common/types.ts"
 import { HashMap } from "$rimbu/hashed/mod.ts"
-import {
-	bgBrightCyan,
-	bgBrightRed,
-	bgBrightYellow,
-	dim as dimmed,
-	gray,
-	rgb24,
-} from "$std/fmt/colors.ts"
-import { slidingWindows } from "$std/collections/sliding_windows.ts"
+import { bgBrightYellow, dim as dimmed, gray, rgb24 } from "$std/fmt/colors.ts"
 import { match } from "npm:ts-pattern"
 import { Stream } from "$rimbu/stream/mod.ts"
 
@@ -44,7 +35,6 @@ const parse = (x: string): Mirrors => {
 	return HashMap.of<Pos, Mirror>(...xs as ArrayNonEmpty<[Pos, Mirror]>)
 }
 
-const isVertical = (dir: Dir): boolean => dir === "U" || dir === "D"
 const isHorizontal = (dir: Dir): boolean => dir === "L" || dir === "R"
 
 const nextRay = (ray: Ray, mirror: Mirror): Ray[] => {
@@ -87,44 +77,45 @@ type Cmp = (a: Entry, b: Entry) => number
 const cmpX: Cmp = ([a], [b]) => a.x - b.x
 const cmpY: Cmp = ([a], [b]) => a.y - b.y
 
-type Next = (mirrors: Mirrors, ray: Ray) => [Mirrors, Ray[]]
-const mkNext = ({ w, h }: Dim, visits: Visit[]): Next => (mirrors, { pos: { y, x }, dir }) => {
-	if (
-		(dir === "U" && y === 0) ||
-		(dir === "D" && y === h - 1) ||
-		(dir === "L" && x === 0) ||
-		(dir === "R" && x === w - 1)
-	) {
-		visits.push([{ y, x }, { y, x }, dir])
-		return [mirrors, []]
-	}
+const mkNext =
+	({ w, h }: Dim, visits: Visit[]) =>
+	(mirrors: Mirrors, { pos: { y, x }, dir }: Ray): [Mirrors, Ray[]] => {
+		if (
+			(dir === "U" && y === 0) ||
+			(dir === "D" && y === h - 1) ||
+			(dir === "L" && x === 0) ||
+			(dir === "R" && x === w - 1)
+		) {
+			visits.push([{ y, x }, { y, x }, dir])
+			return [mirrors, []]
+		}
 
-	const hs = mirrors.stream().filterPure(([m, { type }]) => m.y === y && type !== "-")
-	const vs = mirrors.stream().filterPure(([m, { type }]) => m.x === x && type !== "|")
+		const hs = mirrors.stream().filterPure(([m, { type }]) => m.y === y && type !== "-")
+		const vs = mirrors.stream().filterPure(([m, { type }]) => m.x === x && type !== "|")
 
-	const result = match(dir)
-		.with("R", () => hs.filter(([m]) => x < m.x).minBy(cmpX))
-		.with("L", () => hs.filter(([m]) => x > m.x).maxBy(cmpX))
-		.with("U", () => vs.filter(([m]) => y > m.y).maxBy(cmpY))
-		.with("D", () => vs.filter(([m]) => y < m.y).minBy(cmpY))
-		.exhaustive()
-
-	if (result === undefined) {
-		const end = match(dir)
-			.with("U", () => ({ y: 0, x }))
-			.with("D", () => ({ y: h - 1, x }))
-			.with("L", () => ({ y, x: 0 }))
-			.with("R", () => ({ y, x: w - 1 }))
+		const result = match(dir)
+			.with("R", () => hs.filter(([m]) => x < m.x).minBy(cmpX))
+			.with("L", () => hs.filter(([m]) => x > m.x).maxBy(cmpX))
+			.with("U", () => vs.filter(([m]) => y > m.y).maxBy(cmpY))
+			.with("D", () => vs.filter(([m]) => y < m.y).minBy(cmpY))
 			.exhaustive()
-		visits.push([{ y, x }, end, dir])
-		return [mirrors, []]
-	}
-	const [at, mirror] = result
 
-	const visited = mirrors.set(at, { ...mirror, [dir]: true })
-	visits.push([{ y, x }, at, dir])
-	return [visited, nextRay({ pos: at, dir }, mirror)]
-}
+		if (result === undefined) {
+			const end = match(dir)
+				.with("U", () => ({ y: 0, x }))
+				.with("D", () => ({ y: h - 1, x }))
+				.with("L", () => ({ y, x: 0 }))
+				.with("R", () => ({ y, x: w - 1 }))
+				.exhaustive()
+			visits.push([{ y, x }, end, dir])
+			return [mirrors, []]
+		}
+		const [at, mirror] = result
+
+		const visited = mirrors.set(at, { ...mirror, [dir]: true })
+		visits.push([{ y, x }, at, dir])
+		return [visited, nextRay({ pos: at, dir }, mirror)]
+	}
 
 const arrows = {
 	U: "^",
@@ -138,13 +129,14 @@ const verticalLine = (a: Pos, b: Pos): Pos[] =>
 		y: Math.min(a.y, b.y) + i,
 		x: a.x,
 	}))
+
 const horizontalLine = (a: Pos, b: Pos): Pos[] =>
 	Array.from({ length: Math.abs(a.x - b.x) + 1 }, (_, i) => ({
 		y: a.y,
 		x: Math.min(a.x, b.x) + i,
 	}))
 
-const display = (w: number, h: number) => (mirrors: Mirrors, visits: Visit[]): string => {
+const display = ({ w, h }: Dim, mirrors: Mirrors, visits: Visit[]): string => {
 	const xss = Array.from({ length: h }, () => Array.from({ length: w }, () => gray(".")))
 
 	mirrors.forEach(([pos, mirror]) => {
@@ -157,13 +149,17 @@ const display = (w: number, h: number) => (mirrors: Mirrors, visits: Visit[]): s
 		const arrow = rgb24(arrows[d], { r: 155 + 100 * per, g: 50 + 150 * per, b: 120 * per })
 		const lines = isHorizontal(d) ? horizontalLine : verticalLine
 
-		lines(p, c).forEach((p) => xss[p.y][p.x] = arrow)
+		lines(p, c)
+			.filter(({ y, x }) => 0 <= y && y < h && 0 <= x && x < w)
+			.forEach((p) => xss[p.y][p.x] = arrow)
 	})
 
 	return xss.map((xs) => xs.join("")).join("\n")
 }
 
-const run = (mirrors: Mirrors, next: Next, init: Ray): Mirrors => {
+const run = (mirrors: Mirrors, dim: Dim, init: Ray) => {
+	const visits: Visit[] = []
+	const next = mkNext(dim, visits)
 	const queue = [init]
 
 	while (queue.length) {
@@ -173,34 +169,56 @@ const run = (mirrors: Mirrors, next: Next, init: Ray): Mirrors => {
 		queue.push(...nextRays)
 		mirrors = nextMirrors
 	}
-	return mirrors
+	return { mirrors, visits }
 }
 
-const energized = (w: number, h: number, visits: Visit[]): number => {
+const energized = ({ w, h }: Dim, visits: Visit[]): number => {
 	const dumbGrid = Array.from({ length: h }, () => Array.from({ length: w }, () => "."))
 	visits.forEach(([a, b]) => {
 		const lines = a.y === b.y ? horizontalLine : verticalLine
-		lines(a, b).forEach(({ y, x }) => dumbGrid[y][x] = "#")
+		lines(a, b)
+			.filter(({ y, x }) => 0 <= y && y < h && 0 <= x && x < w)
+			.forEach(({ y, x }) => (dumbGrid[y][x] = "#"))
 	})
 	const energized = dumbGrid.flat().reduce((acc, x) => acc + (x === "#" ? 1 : 0), 0)
 	return energized
 }
 
 const part1impl = (x: string) => {
-	const visits: Visit[] = []
-
 	const ts = x.split("\n")
-	const w = ts[0].length
-	const h = ts.length
-	const mirrors = run(parse(x), mkNext({ w, h }, visits), { pos: { y: 0, x: -1 }, dir: "R" })
+	const dim = { w: ts[0].length, h: ts.length }
+	const { mirrors, visits } = run(parse(x), dim, { pos: { y: 0, x: -1 }, dir: "R" })
 
-	return { render: () => display(w, h)(mirrors, visits), energy: energized(w, h, visits) }
+	return { render: () => display(dim, mirrors, visits), energy: energized(dim, visits) }
+}
+
+export const part2 = (x: string) => {
+	const ts = x.split("\n")
+	const { w, h } = { w: ts[0].length, h: ts.length }
+
+	const up = horizontalLine({ y: -1, x: 0 }, { y: -1, x: w - 1 })
+	const down = horizontalLine({ y: h, x: 0 }, { y: h, x: w - 1 })
+	const left = verticalLine({ y: 0, x: -1 }, { y: h - 1, x: -1 })
+	const right = verticalLine({ y: 0, x: w }, { y: h - 1, x: w })
+	const mirrors = parse(x)
+
+	const energy = Stream
+		.of<[Dir, Pos[]]>(["D", up], ["U", down], ["R", left], ["L", right])
+		.flatMap(([dir, xs]) => xs.map((x) => [dir, x] as const))
+		.map(([dir, pos]) => run(mirrors, { w, h }, { pos, dir }))
+		.map(({ mirrors, visits }) => {
+			const energy = energized({ w, h }, visits)
+
+			console.log(display({ w, h }, mirrors, visits))
+			console.log(energy)
+			return energy
+		})
+		.max()!
+
+	return energy
 }
 
 export const part1 = (x: string): number => part1impl(x).energy
-
-// export const part2 = (x: string): number => {
-// }
 
 if (import.meta.main) {
 	const actual = await input(import.meta)
@@ -210,8 +228,6 @@ if (import.meta.main) {
 	const { render, energy } = part1impl(text)
 	console.log(render())
 	console.log(energy)
-	// const steps = next(mirrors, { pos: { y: 0, x: 0 }, dir: "R" })
-	// console.log(steps[1])
-	// console.log(display(10, 10, steps[0]))
-	// console.log(xs.toJSON())
+
+	console.log(part2(text))
 }
--- ts/day16/main_test.ts
+++ ts/day16/main_test.ts
@@ -1,9 +1,11 @@
 import { input } from "$utils/mod.ts"
 import { assertEquals } from "$std/assert/assert_equals.ts"
-import { part1 } from "./main.ts"
+import { part1, part2 } from "./main.ts"
 
 const text = await input(import.meta)
 
 Deno.test("day 16", async (t) => {
 	await t.step("part 1", () => assertEquals(part1(text), 6622))
+    // takes 4 minutes to run because inefficiency
+    // await t.step("part 2", () => assertEquals(part2(text), 7130))
 })
